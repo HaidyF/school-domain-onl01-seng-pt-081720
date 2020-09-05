a6e269cbb9e5f74d302022cc5da67d54c3d7738f
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-05 03:02:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,7 @@ class School
     @name= name
     @roster= {}
   end
-  def add_student=(student, level)
+  def add_student (student, level)
     roster[level] ||= []
     roster[level] << student
   end
