249a5145927168fced99efb1d4f3d9d8194c5db1
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-05 02:50:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  def initialize
+    school.roster{}
+  end
+  def add_student=(name, Grade)
+    @add_student=add_student
+  end
+  def add_student
+    @add_student
+  end
+
+end
+school = school.new("Bayside High School")
+school.add_student("Zach Morris")
